e5d81d2ff81e8f33f9d821989d65caba3579f729
harley-travis <harley.travis2@gmail.com>
2019-09-11 01:33:13 -0600

    added channel view and functinality and pre form submission

---
+++ resources/views/channels/show.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ $channel->name }}</div>
+
+                <div class="card-body">
+                    
+                    <form id="update-channel-form" action="{{ route('channels.update', $channel->id) }}" method="POST" enctype="multipart/form-data">
+
+                        @csrf
+                        @method('PATCH')
+
+                        <div class="form-group row justify-content-center">
+                            <div class="channel-avatar">
+                                <div onclick="document.getElementById('image').click()" class="channel-avatar-overlay">
+                                    <!-- insert image here -->
+                                </div>
+                            </div>
+                        </div>
+
+                        <input onchange="document.getElementById('update-channel-form').submit()" type="file" name="image" id="image" class="d-none">
+
+                        <div class="form-group">
+                            <label for="name" class="form-control-label">Name</label>
+                            <input id="name" name="name" value="{{ $channel->name }}" type="text" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="description" class="form-control-label">Description</label>
+                            <textarea name="description" id="description" rows="3" class="form-control">{{ $channel->description }}</textarea>
+                        </div>
+
+                        <button class="btn btn-primary" type="submit">Update Channel</button>
+
+                    </form> 
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
