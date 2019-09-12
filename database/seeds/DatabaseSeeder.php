7f2f906ddf29e325df8d3ab1bd78fd32f2d6eb3c
harley-travis <harley.travis2@gmail.com>
2019-09-12 00:37:00 -0600

    init seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -9,8 +10,17 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         // $this->call(UsersTableSeeder::class);
+
+        DB::table('users')->insert([
+            'id' => 'a9c9e1ad-a496-4653-b71f-746946490d50',
+            'name' => 'Tony Stark',
+            'email' =>'tony@gmail.com',
+            'password' => bcrypt('test'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
     }
 }
